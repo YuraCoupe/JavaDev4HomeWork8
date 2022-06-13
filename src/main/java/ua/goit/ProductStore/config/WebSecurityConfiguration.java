998eb8090cf80f14537fca8d4643ab6135cca811
YuraCoupe <yurik124@gmail.com>
2022-06-13 16:18:50 +0300

    remove http configure secure

--- src/main/java/ua/goit/ProductStore/config/WebSecurityConfiguration.java
+++ src/main/java/ua/goit/ProductStore/config/WebSecurityConfiguration.java
@@ -25,10 +25,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http
-                .requiresChannel()
-                .anyRequest()
-                .requiresSecure();
+
         http
                 .authorizeRequests()
                 .antMatchers("/login", "/users/save", "/users/new").permitAll()
