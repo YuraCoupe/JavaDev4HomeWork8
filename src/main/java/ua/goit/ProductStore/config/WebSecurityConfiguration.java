c9dff43598d444fe2d1a3cf3bff083a523111852
YuraCoupe <yurik124@gmail.com>
2022-06-13 15:52:11 +0300

    add http configure secure

--- src/main/java/ua/goit/ProductStore/config/WebSecurityConfiguration.java
+++ src/main/java/ua/goit/ProductStore/config/WebSecurityConfiguration.java
@@ -3,7 +3,6 @@ package ua.goit.ProductStore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,7 +14,6 @@ import ua.goit.ProductStore.service.UsersDetailsService;
 
 @Configuration
 @EnableWebSecurity
-//@Profile("https")
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     private final UsersDetailsService usersDetailsService;
@@ -27,6 +25,10 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
+
         http
                 .authorizeRequests()
                 .antMatchers("/login", "/users/save", "/users/new").permitAll()
