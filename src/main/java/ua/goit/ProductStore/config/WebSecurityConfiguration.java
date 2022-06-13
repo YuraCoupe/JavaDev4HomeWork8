ecca7867830b4c051d3308808dd5e0a63ae5f699
YuraCoupe <yurik124@gmail.com>
2022-06-13 14:57:43 +0300

    Force the use of HTTPS by modifying WebSecurityConfiguration

--- src/main/java/ua/goit/ProductStore/config/WebSecurityConfiguration.java
+++ src/main/java/ua/goit/ProductStore/config/WebSecurityConfiguration.java
@@ -48,6 +48,9 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
+        http.requiresChannel()
+                .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+                .requiresSecure();
     }
 
     @Bean
